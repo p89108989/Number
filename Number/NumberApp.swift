452fea8d5ecb81a6420e82f64907e72770d3bb2d
p89108989 <p881021@gmail.com>
2022-04-26 18:01:24 +0800

    Initial Commit

---
+++ Number/NumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberApp.swift
+//  Number
+//
+//  Created by Thomas on 2022/4/26.
+//
+
+import SwiftUI
+
+@main
+struct NumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
